3a505e1f9230ef1568a2e95ce575e1bb03d051f0
David Kormushoff <kormie@gmail.com>
2011-07-19 14:26:47 -0500

    Initial Commit

---
+++ PeepNews/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeepNews
+//
+//  Created by David Kormushoff on 7/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
